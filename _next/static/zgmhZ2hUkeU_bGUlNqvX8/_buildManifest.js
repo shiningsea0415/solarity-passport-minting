519d919970e0d1c76d301391f76393c93557e430
shiningsea0415 <ski.britness@gmail.com>
2022-09-07 13:46:44 +0700

    update

---
+++ _next/static/zgmhZ2hUkeU_bGUlNqvX8/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-52ddab04a76f2412b6ba.js"],"/_error":["static/chunks/pages/_error-077790e38fbfe3891eb2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
